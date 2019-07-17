665b90b83480ba3c220b812be7f1423dc121fe3e
Steve J. South(NamJungGu) <nowage@gmail.com>
2019-07-17 16:11:04 +0900

    build Script and kubernetes script made.

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+version=$1
+
+if [ ${#version} -eq 0 ];then 
+    version="latest"
+fi
+docker build --rm -t nowage/nginx:$version .
+docker run -d --rm --name n1 -p 8888:80 nowage/nginx:$version
+curl http://127.0.0.1:8888 
+#  docker-machine ip  →        curl http://192.168.99.100:8888
+#                               curl http://192.168.99.101:8888
+docker stop n1
+
+read  -p "Push OK? (Y/n) " yn
+if [ $yn == "n" ]; then
+    echo "Push Cancel.."
+else
+    docker login
+    docker push nowage/nginx:$version    
+fi
--- kubernetes/kubProvision.sh
+++ kubernetes/kubProvision.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+# kubectl delete deploy/nginx1; kubectl run nginx1 --image=nowage/nginx --port=80 -o yaml > /vagrant/MsaPrj/kubernetes/deploy.yaml
+# kubectl scale deployment nginx --replicas=4
+kubectl delete deploy/nginx1
+kubectl create -f ./deploy.yaml
+
+# kubectl expose deployment/nginx --type="NodePort" --port 80
+
+
+# kubectl delete deploy/nginx1
